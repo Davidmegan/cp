8e248cefe46d4c6fb9356145852dec2228f1e788
David Megan C <118439333+Davidmegan@users.noreply.github.com>
2024-01-12 20:02:26 +0530

    Create palindrome_number.py

---
+++ palindrome_number.py
@@ -0,0 +1,7 @@
+//Palindrome number
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x >= 0:
+            return x == int(str(x)[::-1])
+        else:
+            return False

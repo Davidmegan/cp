90febdaaf6975d2055eba1c3fab185ccf52fe98c
David Megan C <118439333+Davidmegan@users.noreply.github.com>
2024-01-12 20:06:07 +0530

    Create two_sum.cpp

---
+++ two_sum.cpp
@@ -0,0 +1,18 @@
+// two sum
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> mp;
+        int complement;
+        for(int i = 0; i<nums.size();i++) {
+            complement = target - nums[i];
+            if(mp.count(complement)) {
+                return {mp[complement],i};
+            }
+            else{
+                mp[nums[i]]=i;
+            }
+        }
+        return{};
+    }
+};

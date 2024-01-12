9c8aa08be957fb949bcd753de6d0937e2adeb159
David Megan C <118439333+Davidmegan@users.noreply.github.com>
2024-01-12 20:07:52 +0530

    Create contains_duplicate.cpp

---
+++ contains_duplicate.cpp
@@ -0,0 +1,11 @@
+// contains duplicate
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        std::sort(nums.begin(),nums.end());
+        for(int i = 1; i < nums.size(); i++){
+            if(nums[i-1]==nums[i]) return true;
+        }
+        return false;
+    }
+};

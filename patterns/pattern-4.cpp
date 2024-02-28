5b315160a9acd05c7d223f3bd7d523c00ae42e00
David Megan C <cdavidmegan@gmail.com>
2024-02-28 20:54:30 +0530

    added 5 patters

---
+++ patterns/pattern-4.cpp
@@ -0,0 +1,17 @@
+/*
+
+1
+2 2
+3 3 3
+
+*/
+
+void triangle(int n) {
+	// Write your code here
+	for(int i=1; i<=n; i++){
+		for(int j=1;j<=i; j++){
+			cout << i << " ";
+		}
+		cout << "\n";
+	}
+}
